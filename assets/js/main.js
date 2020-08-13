322c8c28d2ac72a529d22f8ec87623bd2a45a5a4
cyoung43 <55853741+cyoung43@users.noreply.github.com>
2020-08-12 22:06:21 -0600

    typed updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,6 +16,7 @@
     }
   });
 
+
   // Back to top button
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
@@ -30,6 +31,19 @@
     }, 1500, 'easeInOutExpo');
     return false;
   });
+
+    // Hero typed
+    if ($('.typed').length) {
+      var typed_strings = $(".typed").data('typed-items');
+      typed_strings = typed_strings.split(',')
+      new Typed('.typed', {
+        strings: typed_strings,
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 2000
+      });
+    }
 
   // Header scroll class
   $(window).scroll(function() {
